e0f2bd48ad209413198c0579695adefef91bf99d
ramsagar17 <150685739+ramsagar17@users.noreply.github.com>
2026-07-04 12:00:08 +0530

    Implement StudentController with createStudent endpoint

--- src/main/java/in/coderarmy/controller/StudentController.java
+++ src/main/java/in/coderarmy/controller/StudentController.java
@@ -1,1 +1,21 @@
-StudentController.java
+package in.coderarmy.controller;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/students")
+public class StudentController {
+
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @PostMapping("/create")
+    public Student createStudent(@RequestBody Student student) {
+        return studentService.createStudent(student);
+    }
